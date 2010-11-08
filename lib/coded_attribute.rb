a7546679c5c72c954669c947ba00a1847db37324
Jaden Carver <jaden@upillar.com>
2010-11-08 11:19:27 -0700

    Implemented the syntax

--- lib/coded_attribute.rb
+++ lib/coded_attribute.rb
@@ -1,30 +1,34 @@
 # CodedAttributes
 
 module CodedAttribute
-  def coded_attribute_set(method, *attributes_and_codes)
-  end
 
-  def coded_attribute(method, *attributes_and_codes)
+  def coded_attribute(*attributes_and_codes)
 
-    if attributes_and_codes.last.class == Hash
+    if attributes_and_codes.last.is_a? Hash
       codes = attributes_and_codes.pop
-    elsif attributes_and_codes.last.class == Array
-      if columns_hash[method].sql_type =~ /^(ENUM|SET)\(.*\)$/
-        codes = attributes_and_codes.pop.inject({}) { |h, v| h.merge! v.to_s => v.to_sym }
-      else
-        codes = attributes_and_codes.pop.inject({}) { |h, v| h.merge! h.keys.count => v }
+    elsif attributes_and_codes.last.is_a? Array
+      codes = attributes_and_codes.pop.inject({}) { |h, v| h.merge! h.keys.count => v }
+    end
+
+    attributes = attributes_and_codes.inject({}) do |hash, attribute|
+      hash.merge! attribute.is_a? Hash ? attribute :  { attribute => "#{attribute}_code" }
+    end
+
+    attributes.each_pair do |method, attribute|
+      class_variable_set :"@@#{method}_codes", codes
+      cattr_reader :"#{method}_codes"
+
+      define_method :"#{method}" do
+        self.class.class_variable_get("@@#{method}_codes")[read_attribute(attribute)]
       end
-    end
 
-    class_variable_set :"@@#{method}_codes", codes
-    cattr_reader :"#{method}_codes"
+      define_method :"#{method}=" do |value|
+        write_attribute(attribute, self.class.class_variable_get("@@#{method}_codes").key(value.to_sym))
+      end
+    end # attributes.each_pair
+  end # def coded_attribute
 
-    define_method :"#{method}" do
-      self.class.class_variable_get("@@#{method}_codes")[read_attribute(attribute)]
-    end
+  def coded_attribute_set(method, *attributes_and_codes)
+  end # def coded_attribute_set
 
-    define_method :"#{method}=" do |value|
-      write_attribute(attribute, self.class.class_variable_get("@@#{method}_codes").key(value.to_sym))
-    end
-  end # def coded
 end # module CodedAttributes
